5e86c7e0da25dba2cdb94470aa0e4302d98db879
ilikecoffee18 <cherish.kim@students.makeschool.com>
2019-07-17 18:25:09 -0700

    simple green circle lol, page 10

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 /* ES5 syntax */
+/* I refuse to use semicolons~~~ and you can't make me nyanyanya~~~~ */
 
 (function() {
     'use strict' //this, along with 'function()', are to help prevent conflicts? k
 
     paper.install(window) //installs Paper.js globally
-    paper.setup(document.getElementById('mainCanvas')); //attaches Paper.js to canvas
+    paper.setup(document.getElementById('mainCanvas')) //attaches Paper.js to canvas
 
-    //TODO
+    /* Creates a circle object with 3 args: x, y, and radius */
+    var c = Shape.Circle(200, 200, 50) //var?? not const??? omg omg omg why no omg
+    c.fillColor = 'green'
 
     paper.view.draw() //tells Paper.js to actually draw the thing
 
